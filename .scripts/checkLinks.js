bec4e7e64a8a2d5c7f60c8daf633791eb2261235
pissang <bm2736892@gmail.com>
2021-04-23 17:05:36 +0800

    chore: add link check workflow

---
+++ .scripts/checkLinks.js
@@ -0,0 +1,99 @@
+// jsdelivr CDN may have possibility that having 403 links. This scripts check if any resource is 403 and use purge API to update it.
+
+const pathTool = require('path');
+const globby = require('globby');
+const {runTasks} = require('./task');
+const fetch = require('node-fetch').default;
+
+const rootDir = pathTool.resolve(__dirname, '../');
+
+function getCdnUrl(fileUrl) {
+    return `https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/${fileUrl}`;
+}
+
+function getPurgeUrl(fileUrl) {
+    return `https://purge.jsdelivr.net/gh/apache/echarts-website@asf-site/${fileUrl}`;
+}
+// Only reject when url is 403. Don't check if url is 404 or other status
+function checkIsUrl403(url) {
+    return fetch(url, {
+        method: 'HEAD'
+    }).then(response => {
+        console.log('Check', url, response.status);
+        if (response.status === 403) {
+            throw '403 URL';
+        }
+    });
+}
+
+function purgeUrl(url) {
+    return fetch(url).then(response => response.json())
+        .then(json => {
+            if (json.success) {
+                console.log('Purge Success', url);
+            }
+            else {
+                console.error('Purge Fail', url);
+            }
+        });
+}
+
+async function find403Urls(files) {
+    const totalLen = files.length;
+    let finished = 0;
+    return (await runTasks(files, async (filePath) => {
+        let is403 = false;
+        try {
+            await checkIsUrl403(getCdnUrl(filePath));
+        }
+        catch {
+            is403 = true;
+        }
+        finished++;
+        console.log(`${finished} / ${totalLen} (${(finished / totalLen * 100).toFixed(0)}%)`);
+
+        return is403 ? filePath : '';
+    }, 10)).filter(a => !!a);
+}
+
+async function run() {
+    const files = await globby([
+        'zh/**/*',
+        'examples/**/*',
+        '!examples/en/**/*',    // Not check english content because they don't use jsdelivr
+        '!zh/**/*.html',
+        '!examples/**/*.html',
+
+        // It's won't be uploaded to github
+        '!examples/data/option',
+        '!examples/data-gl/option'
+    ], {
+        cwd: rootDir
+    });
+
+    const files403 = await find403Urls(files);
+    let successPurged = 0;
+    if (files403.length) {
+        console.log(`Purging ${files403.length} 403 Urls`);
+
+        const totalLen = files403.length;
+        let finished = 0;
+        await runTasks(files403, async(filePath) => {
+            try {
+                await purgeUrl(getPurgeUrl(filePath));
+                successPurged++;
+            }
+            catch (e) {
+                console.log('Purge Error', filePath);
+                console.log(e);
+            }
+            finished++;
+            console.log(`${finished} / ${totalLen} (${(finished / totalLen * 100).toFixed(0)}%)`)
+        }, 10);
+    }
+    console.log('Finished');
+    console.log(`Found ${files403.length} in ${files.length} is 403`);
+    console.log(`Successed purged ${successPurged}`);
+}
+
+run();
---
+++ .scripts/task.js
@@ -0,0 +1,51 @@
+function runTasks(
+    taskParamsLists, createTask, concurrency
+) {
+    concurrency = Math.min(taskParamsLists.length, concurrency);
+    return new Promise((resolve, reject) => {
+        let runningTaskCount = 0;
+        let cursor = 0;
+        let rets = [];
+
+        function finishTask(res, idx) {
+            rets[idx] = res;
+            processNext();
+        }
+
+        function failTask(e) {
+            console.error(e);
+            processNext();
+        }
+
+        function processNext() {
+            runningTaskCount--;
+            addTask();
+
+            if (runningTaskCount === 0) {
+                resolve(rets);
+            }
+        }
+
+        function addTask() {
+            const param = taskParamsLists[cursor];
+            if (param) {
+                const currentTaskIdx = cursor;
+                runningTaskCount++;
+                createTask(param)
+                    .then((res) => finishTask(res, currentTaskIdx))
+                    .catch(failTask);
+                cursor++;
+            }
+        }
+
+        for (let i = 0; i < concurrency; i++) {
+            addTask();
+        }
+
+        if (!runningTaskCount) {
+            resolve(rets);
+        }
+    });
+}
+
+module.exports.runTasks = runTasks;
