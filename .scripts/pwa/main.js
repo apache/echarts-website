4f4091258e3757c19794d1c6b7120ba5faa18ed5
plainheart <yhen@all-my-life.cn>
2022-04-24 00:18:01 +0800

    feat(pwa): add offline tip & optimize toast style

--- .scripts/pwa/main.js
+++ .scripts/pwa/main.js
@@ -1,7 +1,6 @@
 import { registerSW } from 'virtual:pwa-register'
 import i18n from './i18n.json'
-// TODO minify
-import styleCSS from './style.css?raw'
+import styleCSS from './style.css?inline'
 
 window.addEventListener('load', () => {
     const lang = i18n[window.EC_WWW_LANG || 'en']
@@ -13,26 +12,39 @@ window.addEventListener('load', () => {
     const pwaToast = document.createElement('div')
     pwaToast.className = 'pwa-toast'
     pwaToast.setAttribute('role', 'alert')
-    pwaToast.innerHTML = `<div class="pwa-msg">${lang['NewContent']}</div><button id="pwa-close">${lang['Close']}</button><button id="pwa-refresh">${lang['Reload']}</button>`
+    pwaToast.innerHTML = `<div class="pwa-msg"></div><button class="pwa-close">${lang['Close']}</button><button class="pwa-refresh">${lang['Reload']}</button>`
     document.body.appendChild(pwaToast)
 
-    const pwaCloseBtn = pwaToast.querySelector('#pwa-close')
-    const pwaRefreshBtn = pwaToast.querySelector('#pwa-refresh')
+    const pwaMsg = pwaToast.querySelector('.pwa-msg')
+    const pwaCloseBtn = pwaToast.querySelector('.pwa-close')
+    const pwaRefreshBtn = pwaToast.querySelector('.pwa-refresh')
 
     let refreshSW
+    let needRefresh
 
-    const refreshCallback = () => refreshSW && refreshSW(true)
+    const refreshCallback = () => {
+        refreshSW && refreshSW(true)
+        needRefresh = false
+    }
 
     const hideToast = () => {
         pwaToast.classList.remove('show')
     }
 
-    const showToast = () => {
+    const showToast = (msg, isRefresh) => {
+        pwaMsg.innerText = msg
+        pwaToast.classList[isRefresh ? 'add' : 'remove']('is-refresh')
         pwaToast.classList.add('show')
     }
 
     pwaRefreshBtn.addEventListener('click', refreshCallback)
     pwaCloseBtn.addEventListener('click', hideToast)
+
+    const onOffline = () => needRefresh || showToast(lang['Offline'])
+    navigator.onLine || onOffline()
+
+    window.addEventListener('online', () => needRefresh || hideToast())
+    window.addEventListener('offline', onOffline)
 
     refreshSW = registerSW({
         immediate: true,
@@ -41,7 +53,8 @@ window.addEventListener('load', () => {
         },
         onNeedRefresh() {
             console.log('New content available')
-            showToast()
+            needRefresh = true
+            showToast(lang['NewContent'], true)
         },
         onRegisterError(e) {
             console.error('failed to register service worker', e)
