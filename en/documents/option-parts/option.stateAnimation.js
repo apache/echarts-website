24453fdcad179d5c1da3373d5340c0d7f46dd7f5
plainheart <plainheart@users.noreply.github.com>
2022-08-03 06:47:32 +0000

    Deploying to asf-site from @ apache/echarts-website@495b120efd9b58d42b397f69c0de27b5b4def958 🚀

--- en/documents/option-parts/option.stateAnimation.js
+++ en/documents/option-parts/option.stateAnimation.js
@@ -1,8 +1,1 @@
-window.__EC_DOC_option_stateAnimation = {
-  "duration": {
-    "desc": "<p>Duration of animation. Animation will be disabled when set to 0.</p>\n"
-  },
-  "easing": {
-    "desc": "<p>Easing of animation.</p>\n"
-  }
-}
+window.__EC_DOC_option_stateAnimation = {"duration":{"desc":"<p>Duration of animation. Animation will be disabled when set to 0.</p>\n"},"easing":{"desc":"<p>Easing of animation.</p>\n"}}
