129dd14dfc7fe5d0adccedd002c6f44b2ea81857
plainheart <plainheart@users.noreply.github.com>
2022-11-11 10:59:06 +0000

    Deploying to asf-site from @ apache/echarts-website@16d9ccb3c89a588b38aa61c131e7a3f1159260c6 🚀

--- examples/examples/js/doc-example/text-block-fragment.js
+++ examples/examples/js/doc-example/text-block-fragment.js
@@ -5,43 +5,41 @@ option = {
       data: [[0, 0]],
       symbolSize: 1,
       label: {
-        normal: {
-          show: true,
-          formatter: [
-            'The whole box is a {term|Text Block}, with',
-            'red border and grey background.',
-            '{fragment1|A Text Fragment} {fragment2|Another Text Fragment}',
-            'Text fragments can be customized.'
-          ].join('\n'),
-          backgroundColor: '#eee',
-          // borderColor: '#333',
-          borderColor: 'rgb(199,86,83)',
-          borderWidth: 2,
-          borderRadius: 5,
-          padding: 10,
-          color: '#000',
-          fontSize: 14,
-          shadowBlur: 3,
-          shadowColor: '#888',
-          shadowOffsetX: 0,
-          shadowOffsetY: 3,
-          lineHeight: 30,
-          rich: {
-            term: {
-              fontSize: 18,
-              color: 'rgb(199,86,83)'
-            },
-            fragment1: {
-              backgroundColor: '#000',
-              color: 'yellow',
-              padding: 5
-            },
-            fragment2: {
-              backgroundColor: '#339911',
-              color: '#fff',
-              borderRadius: 15,
-              padding: 5
-            }
+        show: true,
+        formatter: [
+          'The whole box is a {term|Text Block}, with',
+          'red border and grey background.',
+          '{fragment1|A Text Fragment} {fragment2|Another Text Fragment}',
+          'Text fragments can be customized.'
+        ].join('\n'),
+        backgroundColor: '#eee',
+        // borderColor: '#333',
+        borderColor: 'rgb(199,86,83)',
+        borderWidth: 2,
+        borderRadius: 5,
+        padding: 10,
+        color: '#000',
+        fontSize: 14,
+        shadowBlur: 3,
+        shadowColor: '#888',
+        shadowOffsetX: 0,
+        shadowOffsetY: 3,
+        lineHeight: 30,
+        rich: {
+          term: {
+            fontSize: 18,
+            color: 'rgb(199,86,83)'
+          },
+          fragment1: {
+            backgroundColor: '#000',
+            color: 'yellow',
+            padding: 5
+          },
+          fragment2: {
+            backgroundColor: '#339911',
+            color: '#fff',
+            borderRadius: 15,
+            padding: 5
           }
         }
       }
--- examples/examples/js/doc-example/title-block.js
+++ examples/examples/js/doc-example/title-block.js
@@ -11,17 +11,15 @@ option = {
         {
           value: [0, 0],
           label: {
-            normal: {
-              formatter: [
-                '{tc|Center Title}{titleBg|}',
-                '  Content text xxxxxxxx {sunny|} xxxxxxxx {cloudy|}  ',
-                '{hr|}',
-                '  xxxxx {showers|} xxxxxxxx  xxxxxxxxx  '
-              ].join('\n'),
-              rich: {
-                titleBg: {
-                  align: 'right'
-                }
+            formatter: [
+              '{tc|Center Title}{titleBg|}',
+              '  Content text xxxxxxxx {sunny|} xxxxxxxx {cloudy|}  ',
+              '{hr|}',
+              '  xxxxx {showers|} xxxxxxxx  xxxxxxxxx  '
+            ].join('\n'),
+            rich: {
+              titleBg: {
+                align: 'right'
               }
             }
           }
@@ -29,30 +27,26 @@ option = {
         {
           value: [0, 1],
           label: {
-            normal: {
-              formatter: [
-                '{titleBg|Left Title}',
-                '  Content text xxxxxxxx {sunny|} xxxxxxxx {cloudy|}  ',
-                '{hr|}',
-                '  xxxxx {showers|} xxxxxxxx  xxxxxxxxx  '
-              ].join('\n')
-            }
+            formatter: [
+              '{titleBg|Left Title}',
+              '  Content text xxxxxxxx {sunny|} xxxxxxxx {cloudy|}  ',
+              '{hr|}',
+              '  xxxxx {showers|} xxxxxxxx  xxxxxxxxx  '
+            ].join('\n')
           }
         },
         {
           value: [0, 2],
           label: {
-            normal: {
-              formatter: [
-                '{titleBg|Right Title}',
-                '  Content text xxxxxxxx {sunny|} xxxxxxxx {cloudy|}  ',
-                '{hr|}',
-                '  xxxxx {showers|} xxxxxxxx  xxxxxxxxx  '
-              ].join('\n'),
-              rich: {
-                titleBg: {
-                  align: 'right'
-                }
+            formatter: [
+              '{titleBg|Right Title}',
+              '  Content text xxxxxxxx {sunny|} xxxxxxxx {cloudy|}  ',
+              '{hr|}',
+              '  xxxxx {showers|} xxxxxxxx  xxxxxxxxx  '
+            ].join('\n'),
+            rich: {
+              titleBg: {
+                align: 'right'
               }
             }
           }
@@ -60,53 +54,51 @@ option = {
       ],
       symbolSize: 1,
       label: {
-        normal: {
-          show: true,
-          backgroundColor: '#ddd',
-          borderColor: '#555',
-          borderWidth: 1,
-          borderRadius: 5,
-          color: '#000',
-          fontSize: 14,
-          rich: {
-            titleBg: {
-              backgroundColor: '#000',
-              height: 30,
-              borderRadius: [5, 5, 0, 0],
-              padding: [0, 10, 0, 10],
-              width: '100%',
-              color: '#eee'
-            },
-            tc: {
-              align: 'center',
-              color: '#eee'
-            },
-            hr: {
-              borderColor: '#777',
-              width: '100%',
-              borderWidth: 0.5,
-              height: 0
-            },
-            sunny: {
-              height: 30,
-              align: 'left',
-              backgroundColor: {
-                image: weatherIcons.Sunny
-              }
-            },
-            cloudy: {
-              height: 30,
-              align: 'left',
-              backgroundColor: {
-                image: weatherIcons.Cloudy
-              }
-            },
-            showers: {
-              height: 30,
-              align: 'left',
-              backgroundColor: {
-                image: weatherIcons.Showers
-              }
+        show: true,
+        backgroundColor: '#ddd',
+        borderColor: '#555',
+        borderWidth: 1,
+        borderRadius: 5,
+        color: '#000',
+        fontSize: 14,
+        rich: {
+          titleBg: {
+            backgroundColor: '#000',
+            height: 30,
+            borderRadius: [5, 5, 0, 0],
+            padding: [0, 10, 0, 10],
+            width: '100%',
+            color: '#eee'
+          },
+          tc: {
+            align: 'center',
+            color: '#eee'
+          },
+          hr: {
+            borderColor: '#777',
+            width: '100%',
+            borderWidth: 0.5,
+            height: 0
+          },
+          sunny: {
+            height: 30,
+            align: 'left',
+            backgroundColor: {
+              image: weatherIcons.Sunny
+            }
+          },
+          cloudy: {
+            height: 30,
+            align: 'left',
+            backgroundColor: {
+              image: weatherIcons.Cloudy
+            }
+          },
+          showers: {
+            height: 30,
+            align: 'left',
+            backgroundColor: {
+              image: weatherIcons.Showers
             }
           }
         }
